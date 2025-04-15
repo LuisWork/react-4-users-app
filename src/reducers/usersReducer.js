fb0ee9de9357c4e04821785e0d62065defe27dea
LuisWork <luiszambrano.work@gmail.com>
2025-04-15 17:59:43 -0400

    Escribiendo el Slice y los ACTIONS REDUCER

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -3,7 +3,6 @@ export const usersReducer = (state = [], action) => {
         case 'addUser':
             return [...state, {
                 ...action.payload,
-                //id: new Date().getTime()
             }]
         case 'removeUser':
             return state.filter(user => user.id !== action.payload)
